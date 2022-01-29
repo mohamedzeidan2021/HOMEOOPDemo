d499e3adde7136565c4d89b731cf79ae9c010a77
MohamedZeidan <81834882+mohamedzeidan2021@users.noreply.github.com>
2022-01-29 13:25:44 -0800

    Update Gorg.cpp

--- Gorg.cpp
+++ Gorg.cpp
@@ -30,3 +30,16 @@ void Gorg::getsAttacked()
 		}
 	}
 }
+/*
+The first action is to check if the shield will regen slightly
+15% of the time, add 1 to the shiled value, and also make sure this new shield is clamped to its max val
+85% of the time, no regen will occur (we don't need to do anything for this)
+
+the 2nd action is to check if health damage is going to be done
+35% of the time, the attack misses and does 0 dmg
+65% of the time, the following occurs:
+	shield takes 1-2 random damage
+	if the modified shield value is now less than zero, attack does random 5-10 health damage
+	Think of this as an actual physical shield that can be damaged with use
+	If the shield value is reduced to zero or negative value, that means it has been destroyed
+*/
